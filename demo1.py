7ce9e29d1245a5e5c4788fe85da5a7a1a57262b8
Theodor <58467648+theo-prs@users.noreply.github.com>
2022-11-19 19:36:52 +0200

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,5 +1,5 @@
 import os
-for root, dirs, files in os.walk('/Users/georgiana/Desktop'):
+for root, dirs, files in os.walk('/Users/[username]/Desktop'):
     for file in files:
         if file.endswith('.docx'):
             print(os.path.join(root, file))
